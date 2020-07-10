1e47f206e37e0caa6781509c436005ef9d933313
Pavan Raju <pavanraju023@gmail.com>
2020-07-10 22:26:54 +1000

    TS-22: Add backend API dockerfiles and docker compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,7 +80,8 @@ DATABASES = {
         'NAME': 'toolbox',
         'USER': 'app_user',
         'PASSWORD': 'app_password',
-        'HOST': '0.0.0.0',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
