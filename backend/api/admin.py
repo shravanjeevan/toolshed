02829d2aa228f8e6311bfc1cc180629ef20e0f74
Shravan <shravan.jeevan@hotmail.com>
2020-07-22 20:16:47 +1000

    Updated admin.py to register new models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -3,6 +3,8 @@ from .models import *
 
 admin.site.register(BlogPost)
 admin.site.register(User)
-admin.site.register(UserGroup)
-admin.site.register(BlogPostVisibility)
 admin.site.register(BlogPostTag)
+admin.site.register(BlogPostComment)
+admin.site.register(KnowledgeBaseItem)
+admin.site.register(KnowledgeBaseTag)
+admin.site.register(Tool)
