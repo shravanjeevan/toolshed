9bd6dbcae326547d1d9048a054c23991789a0d52
Jacky Lee <jlee@shieldgeo.com>
2020-07-26 21:02:21 +1000

    initial push of app page components and bundles folder structure

---
+++ app/src/bundles/app-page/components/AppPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import AppHeader from './AppHeader';
+import PopularPosts from './PopularPosts';
+import CategoryExplorer from './CategoryExplorer';
+import TagExplorer from './TagExplorer';
+
+import './AppPage.css';
+
+class AppPage extends React.Component {
+    render() {
+        return (
+            <div className="page">
+                <AppHeader />
+                <div className="row">
+                    <div className="col-lg">
+                        <div className="mb-5">
+                            <PopularPosts />
+                        </div>
+                    </div>
+                    <div className="col-lg">
+                        <div className="mb-5">
+                            <CategoryExplorer />
+                        </div>
+                        <div className="mb-5">
+                            <TagExplorer />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AppPage;
--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AppPage from './bundles/app-page/components/AppPage';
+// import reducers from './reducers';
+
+// const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+        <AppPage />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
