0e20e9c8941942092a9bde2ceb33dc14d3ee623e
Pavan Raju <pavanraju023@gmail.com>
2020-08-12 09:17:24 +1000

    TS-00: Add scripts to add fake data

--- backend/data/load-data.sh
+++ backend/data/load-data.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-curl -X POST -F "username=jdean" -F "password=aStrongPassword" -F "email=jdean@cse.unsw.edu.au" -F "first_name=Jeff" -F "last_name=Dean" localhost:8000/auth/register/
-
-curl -X POST -F "username=morty" -F "password=aStrongPassword" -F "email=morty@cse.unsw.edu.au" -F "first_name=Mortada" -F "last_name=Al-Banna" localhost:8000/auth/register/
-
-curl -X POST -F "username=boualem" -F "password=aStrongPassword" -F "email=bb@cse.unsw.edu.au" -F "first_name=Boualem" -F "last_name=Benatallah" localhost:8000/auth/register/
-
 curl -d "@1.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
 
 curl -d "@2.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
@@ -19,6 +13,10 @@ curl -d "@5.json" -H "Content-Type: application/json" -X POST localhost:8000/pos
 curl -d "@6.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
 
 curl -d "@7.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
+
+curl -d "@8.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
+
+curl -d "@9.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
 
 
 
