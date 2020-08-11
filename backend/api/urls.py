77e0390dcbb7456e9cc795365133a24084d83836
Pavan Raju <pavanraju023@gmail.com>
2020-08-11 23:20:31 +1000

    TS-00: Add knowledge/id endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,32 +11,31 @@ urlpatterns = [
 
     # common
     path('posts/', views.BlogsList.as_view()),
+    path('tags/popular/', views.PopularBlogTags.as_view()),
 
     # posts
     # GET /posts/popular?top=10
     path('posts/popular/', views.PopularBlogList.as_view()),
     path('posts/<int:pk>', views.Blogs.as_view()),
 
-
     # knowledge base
     # GET /knowledge/popular?top=10
     path('knowledge/popular/', views.PopularKnowledgeList.as_view()),
-    # todo: (not yet implemented)
-    # path('posts/knowledge/<int:pk>', views.Blogs.as_view()),
-    # path('posts/knowledge/popular/', views.PopularBlogList.as_view()),
+    path('knowledge/<int:pk>', views.KnowledgeBaseList.as_view()),
 
+    # tools
+    path('tools/', views.Tools.as_view()),
+
+    # tool categories
     path('categories/', views.AllCategories.as_view()),
     path('categories/popular', views.ExploreCategories.as_view()),
     path('categories/<str:category>', views.FilterCategories.as_view()),
 
-    path('tools/', views.Tools.as_view()),
-
-    path('tags/popular/', views.PopularBlogTags.as_view()),
-
+    # search
     re_path(r'search/$', views.Search.as_view()),
 
+    #  auth and admin
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
     path('api/auth/', include('knox.urls')),
 
     path("auth/register/", views.RegistrationAPI.as_view()),
