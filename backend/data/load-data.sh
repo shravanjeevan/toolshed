b9a65dbdef578934bbc597c8b9a3868b0c309ade
Pavan Raju <pavanraju023@gmail.com>
2020-08-11 20:40:56 +1000

    TS-00: Add ability to get top x number of popular blogs or kb items

--- backend/data/load-data.sh
+++ backend/data/load-data.sh
@@ -14,5 +14,11 @@ curl -d "@3.json" -H "Content-Type: application/json" -X POST localhost:8000/pos
 
 curl -d "@4.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
 
+curl -d "@5.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
+
+curl -d "@6.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
+
+curl -d "@7.json" -H "Content-Type: application/json" -X POST localhost:8000/posts/
+
 
 
